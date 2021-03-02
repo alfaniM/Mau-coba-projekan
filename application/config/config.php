1ae55cbfe6d2c92cc43c556bc3da7e433d805256
Alfani <alfaniaulia09@gmail.com>
2021-03-02 13:55:12 +0700

    s

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Mau-coba-projekan/';
+$config['base_url'] = 'http://localhost/pkl-uin/';
 
 /*
 |--------------------------------------------------------------------------
