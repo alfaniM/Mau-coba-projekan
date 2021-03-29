dd4fb98b0821f6f2ee20da95821dfda98cd969e2
Alfani <alfaniaulia09@gmail.com>
2021-03-29 19:32:51 +0700

    Add Persuratan php dummy

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -71,6 +71,21 @@
     });
 </script>
 
+<script>
+    $(document).ready(function() {
+        $("select").change(function() {
+            $(this).find("option:selected").each(function() {
+                var optionValue = $(this).attr("value");
+                if (optionValue) {
+                    $(".box").not("." + optionValue).hide();
+                    $("." + optionValue).show();
+                } else {
+                    $(".box").hide();
+                }
+            });
+        }).change();
+    });
+</script>
 </body>
 
 </html>
